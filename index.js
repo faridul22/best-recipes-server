085bbe18711f1fb14fb097db16c801a11924847b
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-05-03 12:01:07 +0600

    chefs data added and send response chefs data

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
-
 const app = express()
+const port = process.env.PORT || 5000;
 
-const port = 5000;
+const chefs = require('./data/chefData.json')
 
 app.get('/', (req, res) => {
     res.send('Best Recipes is running')
 });
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+})
 
 app.listen(port, () => {
     console.log(`Best Recipes is running on port: ${port}`)
